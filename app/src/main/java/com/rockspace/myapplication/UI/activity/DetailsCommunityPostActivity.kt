5eccba73f3a695c06d564ebb7079e71f6a39c247
Haidil <mohamadhaidilidris@hotmail.com>
2021-09-13 10:43:40 +0800

    - set up ui

---
+++ app/src/main/java/com/rockspace/myapplication/UI/activity/DetailsCommunityPostActivity.kt
@@ -0,0 +1,11 @@
+package com.rockspace.myapplication.UI.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsCommunityPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_community_post)
+    }
+}
--- app/src/main/java/com/rockspace/myapplication/UI/activity/ManageCommunityPostActivity.kt
+++ app/src/main/java/com/rockspace/myapplication/UI/activity/ManageCommunityPostActivity.kt
@@ -4,32 +4,71 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.format.DateFormat.is24HourFormat
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import com.rockspace.myapplication.R
 import com.rockspace.myapplication.databinding.ActivityManageCommunityPostBinding
+import java.util.*
 
 class ManageCommunityPostActivity : AppCompatActivity() {
     private lateinit var binding: ActivityManageCommunityPostBinding
+    private var eventHour = 0
+    private var eventMin = 0
+    private var eventDate = MaterialDatePicker.todayInUtcMilliseconds()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_community_post)
 
-        binding.etTime.setOnClickListener {
-            openTimePicker()
-        }
+        binding.etTime.setOnClickListener { openTimePicker() }
+        binding.etDate.setOnClickListener { openDatePicker() }
+        binding.btnBack.setOnClickListener { onBackPressed() }
+
     }
 
     private fun openTimePicker() {
-        val is24Hour = is24HourFormat(this)
-        val clockFormat = if (is24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
-
         val picker = MaterialTimePicker.Builder()
-            .setTimeFormat(clockFormat)
-            .setHour(12)
-            .setMinute(0)
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(eventHour)
+            .setMinute(eventMin)
             .setTitleText("Set Time")
             .build()
         picker.show(supportFragmentManager, "Tag")
+
+        picker.addOnPositiveButtonClickListener {
+            eventHour = picker.hour
+            eventMin = picker.minute
+
+            val hour = when {
+                picker.hour == 0 -> 12
+                picker.hour > 12 -> picker.hour - 12
+                else -> picker.hour
+            }
+
+            val format = if(picker.hour>12) "pm" else "am"
+
+            binding.etTime.setText("$hour:${picker.minute} $format")
+        }
+    }
+
+    private fun openDatePicker() {
+        val picker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select Date")
+            .setSelection(eventDate)
+            .build()
+
+        picker.show(supportFragmentManager, "Tag")
+
+        picker.addOnPositiveButtonClickListener {
+            eventDate = it
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            calendar.time = Date(it)
+
+            val dateText = "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH)}/" +
+                    "${calendar.get(Calendar.YEAR)}"
+
+            binding.etDate.setText(dateText)
+        }
     }
 }
