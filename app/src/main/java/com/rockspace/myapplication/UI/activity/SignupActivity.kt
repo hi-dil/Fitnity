cdb4cd66c1b6c95dc7f8a982fd6021eb4a3f5c4f
Haidil <mohamadhaidilidris@hotmail.com>
2021-09-14 22:10:14 +0800

    - done creating layout for login and signup activity

---
+++ app/src/main/java/com/rockspace/myapplication/UI/activity/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.rockspace.myapplication.UI.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rockspace.myapplication.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
